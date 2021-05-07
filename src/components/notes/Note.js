c7e5191576c4a22621a1f0abb39db1d25b240ac6
Mr-G-D <daredinesh00@gmail.com>
2021-05-07 23:08:46 +0530

    note component refined

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Note = ({ note }) => {
-  return <div>{note.title}</div>;
+  return (
+    <div className="note white">
+      <div className="right-align">
+        <i
+          style={{ cursor: "pointer" }}
+          className="material-icons red-text"
+        >
+          favorite
+        </i>
+        <i
+          style={{ cursor: "pointer" }}
+          className="material-icons"
+        >
+          delete
+        </i>
+      </div>
+      <h5 className="black-text">{note.title}</h5>
+      <p className="truncate">{note.content}</p>
+      <p className="grey-text">Days ago</p>
+      <div className="right-align">
+        <i className="material-icons">edit</i>
+      </div>
+    </div>
+  );
 };
 
 export default Note;
