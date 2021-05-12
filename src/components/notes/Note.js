d8e846d7054263aa302e8444553b6747f1bf7dfb
Mr-G-D <daredinesh00@gmail.com>
2021-05-12 22:49:04 +0530

    Evernote Clone Officially completed

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -17,38 +17,40 @@ const Note = ({ note }) => {
   };
   const favIcon = note.favorite ? "favorite" : "favorite_border";
   return (
-    <div className="note white">
-      <div className="right-align">
-        <i
-          style={{ cursor: "pointer" }}
-          className="material-icons red-text"
-          onClick={toggleFavHandler}
-        >
-          {favIcon}
-        </i>
-        <i
-          style={{ cursor: "pointer" }}
-          className="material-icons"
-          onClick={deleteNoteHandler}
-        >
-          delete
-        </i>
-      </div>
-      <Link to={"/note/" + note.id}>
-        <h5 className="black-text">{note.title}</h5>
-      </Link>
-      <p className="truncate">{note.content}</p>
-      <p className="grey-text">{moment(note.createdAt.toDate()).fromNow()}</p>
-      <div className="right-align">
-        <Link to={"/edit/" + note.id}>
+    <div className="card container margin">
+      <div className="note white center-align">
+        <div className="right-align">
+          <i
+            style={{ cursor: "pointer" }}
+            className="material-icons red-text"
+            onClick={toggleFavHandler}
+          >
+            {favIcon}
+          </i>
           <i
             style={{ cursor: "pointer" }}
             className="material-icons"
-            onClick={editNoteHandler}
+            onClick={deleteNoteHandler}
           >
-            edit
+            delete
           </i>
+        </div>
+        <Link to={"/note/" + note.id}>
+          <h5 className="black-text">{note.title}</h5>
         </Link>
+        <p className="truncate">{note.content}</p>
+        <p className="grey-text">{moment(note.createdAt.toDate()).fromNow()}</p>
+        <div className="right-align">
+          <Link to={"/edit/" + note.id}>
+            <i
+              style={{ cursor: "pointer" }}
+              className="material-icons"
+              onClick={editNoteHandler}
+            >
+              edit
+            </i>
+          </Link>
+        </div>
       </div>
     </div>
   );
