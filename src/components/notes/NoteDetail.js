80e25ad6e6486ea2b42b611f27d44f8e7a6c28f0
Mr-G-D <daredinesh00@gmail.com>
2021-05-12 20:41:51 +0530

    unknown id cleared

--- src/components/notes/NoteDetail.js
+++ src/components/notes/NoteDetail.js
@@ -14,11 +14,11 @@ const NoteDetail = (props) => {
   const note = useSelector(
     ({ firestore: { data } }) => data.notes && data.notes[id],
   );
-  return (
+  const noteMarkup = !isLoaded(note) ? (
     <div className="container section">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">{note?.title}</span>
+          <span className="card-title">Loading...</span>
           <p>{note?.content}</p>
         </div>
         <div className="card-action grey lighten-4">
@@ -26,7 +26,30 @@ const NoteDetail = (props) => {
         </div>
       </div>
     </div>
+  ) : isEmpty(note) ? (
+    <div className="container section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">OOPSIE!</span>
+          <p>Note not found</p>
+        </div>
+        <div className="card-action grey lighten-4"></div>
+      </div>
+    </div>
+  ) : (
+    <div className="container section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">{note.title}</span>
+          <p>{note.content}</p>
+        </div>
+        <div className="card-action grey lighten-4">
+          <div className="">{moment(note.createdAt.toDate()).calendar()}</div>
+        </div>
+      </div>
+    </div>
   );
+  return noteMarkup;
 };
 
 export default NoteDetail;
