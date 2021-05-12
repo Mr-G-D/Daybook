299ce3b55b41c0e61710f839efe2f2a8405be47c
Mr-G-D <daredinesh00@gmail.com>
2021-05-12 22:33:42 +0530

    edit note action added

--- src/components/home/form.js
+++ src/components/home/form.js
@@ -4,16 +4,8 @@ import { addNote } from "../../store/actions/noteAction";
 import { useDispatch } from "react-redux";
 
 const Form = () => {
-  const [
-    title,
-    bindTitle,
-    resetTitle,
-  ] = useInput();
-  const [
-    content,
-    bindContent,
-    resetContent,
-  ] = useInput();
+  const [title, bindTitle, resetTitle] = useInput();
+  const [content, bindContent, resetContent] = useInput();
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,13 +15,8 @@ const Form = () => {
   };
   return (
     <div className="section">
-      <form
-        onSubmit={handleSubmit}
-        className="white"
-      >
-        <h5 className="grey-text text-darken-3">
-          New Note
-        </h5>
+      <form onSubmit={handleSubmit} className="white">
+        <h5 className="grey-text text-darken-3">New Note</h5>
         <div className="input-field">
           <input
             id="note_title"
@@ -37,10 +24,7 @@ const Form = () => {
             className="validate"
             {...bindTitle}
           />
-          <label
-            className="active"
-            htmlFor="note_title"
-          >
+          <label className="active" htmlFor="note_title">
             Title
           </label>
         </div>
@@ -52,9 +36,7 @@ const Form = () => {
           ></textarea>
           <label htmlFor="body">Body</label>
         </div>
-        <button className="btn green">
-          Submit
-        </button>
+        <button className="btn green">Submit</button>
       </form>
     </div>
   );
--- src/components/notes/EditNote.js
+++ src/components/notes/EditNote.js
@@ -1,9 +1,49 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { updateNote } from "./../../store/actions/noteAction";
+import useInput from "../../customhook/useInput";
+import { useHistory } from "react-router-dom";
 
 const EditNote = () => {
   const note = useSelector((state) => state.note);
-  return <div>Note Editing</div>;
+  const [title, bindTitle, resetTitle] = useInput(note.title);
+  const [content, bindContent, resetcontent] = useInput(note.content);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(updateNote({ id: note.id, title, content }));
+    resetTitle();
+    resetcontent();
+    history.push("/");
+  };
+  return (
+    <div className="section">
+      <form onSubmit={handleSubmit} className="white">
+        <h5 className="grey-text text-darken-3">New Note</h5>
+        <div className="input-field">
+          <input
+            id="note_title"
+            type="text"
+            className="validate"
+            {...bindTitle}
+          />
+          <label className="active" htmlFor="note_title">
+            Title
+          </label>
+        </div>
+        <div className="input-field">
+          <textarea id="body" className="materialize-textarea" {...bindContent}>
+            &nbsp;{" "}
+          </textarea>
+          <label className="active" htmlFor="body">
+            Body
+          </label>
+        </div>
+        <button className="btn green">Update</button>
+      </form>
+    </div>
+  );
 };
 
 export default EditNote;
