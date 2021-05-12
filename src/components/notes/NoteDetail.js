950ce142a5dc196ae9ef7b7796f02f77b61e096f
Mr-G-D <daredinesh00@gmail.com>
2021-05-12 20:19:05 +0530

    note view page rendered

--- src/components/notes/NoteDetail.js
+++ src/components/notes/NoteDetail.js
@@ -1,8 +1,32 @@
 import React from "react";
+import { useFirestoreConnect, isLoaded, isEmpty } from "react-redux-firebase";
+import { useSelector } from "react-redux";
+import moment from "moment";
 
 const NoteDetail = (props) => {
   const id = props.match.params.id;
-  return <div>Note Detail: {id}</div>;
+  useFirestoreConnect([
+    {
+      collection: "notes",
+      doc: id,
+    },
+  ]);
+  const note = useSelector(
+    ({ firestore: { data } }) => data.notes && data.notes[id],
+  );
+  return (
+    <div className="container section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">{note?.title}</span>
+          <p>{note?.content}</p>
+        </div>
+        <div className="card-action grey lighten-4">
+          <div className="">{moment(note?.createdAt.toDate()).calendar()}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NoteDetail;
